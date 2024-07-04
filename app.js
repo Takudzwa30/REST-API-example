f4b5bd57168890bdc70ab19726771642ccb7e29a
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-07-04 22:14:09 +0400

    add headers

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const app = express();
 // application/json
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, PATCH, DELETE"
+  );
+  res.setHeader("Access-COntrol-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
