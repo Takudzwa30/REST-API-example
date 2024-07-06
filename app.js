8af1cda5f9f7e2b6e10b97bcd03bacd87a11cfe5
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-07-06 18:09:37 +0400

    added single post response

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const bodyParser = require("body-parser");
@@ -10,6 +12,8 @@ const app = express();
 
 // application/json
 app.use(bodyParser.json());
+
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -25,6 +29,12 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message });
+});
 
 mongoose
     .connect(
--- controllers/feed.js
+++ controllers/feed.js
@@ -2,33 +2,20 @@ const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
 
-// exports.getPosts = (req, res, next) => {
-//     res.status(200).json({
-//         posts: [
-//             {
-//                 _id: "jsdfkjhdsjfdsf",
-//                 title: "Hello",
-//                 content: "this is what we do, and you wanna know why we do it?",
-//                 imageUrl: "images/duck.jpg",
-//                 creator: {
-//                     name: "Takudzwa Allen",
-//                 },
-//                 createdAt: new Date(),
-//             },
-//         ],
-//     });
-// };
-
 exports.getPosts = (req, res, next) => {
     Post.find()
         .then((posts) => {
             console.log(posts);
             res.status(200).json({
+                message: "Posts fetched",
                 posts: posts,
             });
         })
         .catch((err) => {
-            console.log(err);
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
         });
 };
 
@@ -38,10 +25,9 @@ exports.createPost = (req, res, next) => {
     const content = req.body.content;
 
     if (!errors) {
-        return res.status(422).json({
-            message: "Validation falied, data is incorrect",
-            errors: errors.array(),
-        });
+        const error = new Error("Validation falied, data is incorrect");
+        error.statusCode = 422;
+        throw error;
     }
     const post = new Post({
         title: title,
@@ -62,5 +48,30 @@ exports.createPost = (req, res, next) => {
                 post: result,
             });
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+
+    Post.findById(postId)
+        .then((post) => {
+            if (!post) {
+                const error = new Error("Could not find post.");
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200).json({ message: "Post Fetched", post: post });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 };
