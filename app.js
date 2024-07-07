777a77c59f93708addc414ffd7aff29500830da3
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-07-08 00:10:34 +0400

    init authentication

--- app.js
+++ app.js
@@ -8,64 +8,65 @@ const mongoose = require("mongoose");
 const multer = require("multer");
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 const fileStorage = multer.diskStorage({
-    destination: (req, res, cb) => {
-        cb(null, "images");
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().toISOString() + "-" + file.originalname);
-    },
+  destination: (req, res, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
-    if (
-        file.mimetype === "image/png" ||
-        file.mimetype === "image/jpg" ||
-        file.mimetype === "image/jpeg"
-    ) {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
 };
 
 // application/json
 app.use(bodyParser.json());
 app.use(
-    multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, PUT, POST, PATCH, DELETE"
-    );
-    res.setHeader(
-        "Access-COntrol-Allow-Headers",
-        "Content-Type, Authorization"
-    );
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, PATCH, DELETE"
+  );
+  res.setHeader("Access-COntrol-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
+
 app.use((error, req, res, next) => {
-    console.log(error);
-    const status = error.statusCode || 500;
-    const message = error.message;
-    res.status(status).json({ message: message });
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
 mongoose
-    .connect(
-        "mongodb+srv://takudzwamushai:QtOFVsXgKrdHKl9c@cluster0.fivjgzv.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0"
-    )
-    .then((result) => {
-        app.listen(8080);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+  .connect(
+    "mongodb+srv://takudzwamushai:QtOFVsXgKrdHKl9c@cluster0.fivjgzv.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
